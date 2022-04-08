6d656e845cc436155bee06da5b3a773dc160b18d
DarylMConway <daryl.conway@digital.hmrc.gov.uk>
2022-04-08 11:14:38 +0100

    VER-20732 Update Question Repository Scala version and dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,14 +6,14 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.5.0",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.21.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.62.0",
-    "uk.gov.hmrc"             %% "domain"                     % "6.0.0-play-28",
+    "uk.gov.hmrc"             %% "domain"                     % "8.0.0-play-28",
     "uk.gov.hmrc"             %% "reactive-circuit-breaker"   % "3.5.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.5.0"  % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.21.0"  % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8" % Test,
     "org.scalatestplus.play"  %% "scalatestplus-play"         % "5.1.0"  % Test,
     "org.mockito"             %% "mockito-scala-scalatest"    % "1.7.1"  % Test,
