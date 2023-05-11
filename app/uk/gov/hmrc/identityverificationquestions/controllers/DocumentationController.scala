d485f049c54fa5d8d26fc42f20c5eb13eb8bc55b
drainhmrc <31273776+drainhmrc@users.noreply.github.com>
2023-05-11 10:44:57 +0100

    VER-3006: Add api documentation endpoints WIP

--- app/uk/gov/hmrc/identityverificationquestions/controllers/DocumentationController.scala
+++ app/uk/gov/hmrc/identityverificationquestions/controllers/DocumentationController.scala
@@ -16,21 +16,62 @@
 
 package uk.gov.hmrc.identityverificationquestions.controllers
 
+import akka.stream.Materializer
 import controllers.Assets
+import play.api.Configuration
+import play.api.http.{ContentTypes, MimeTypes}
+
 import javax.inject.{Inject, Singleton}
-import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import play.api.mvc.{Action, AnyContent, Codec, ControllerComponents}
+import play.filters.cors.CORSActionBuilder
+import play.libs.Json
+import uk.gov.hmrc.identityverificationquestions.config.AppConfig
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import views.txt
+
+import scala.concurrent.{ExecutionContext, Future}
 
 
 @Singleton
-class DocumentationController @Inject() (assets: Assets, cc: ControllerComponents) extends BackendController (cc) {
+class DocumentationController @Inject() (assets: Assets, cc: ControllerComponents, configuration: Configuration)(implicit mat: Materializer, ec: ExecutionContext) extends BackendController (cc) {
 
-  def definition(): Action[AnyContent] = {
-    assets.at("/public/api", "definition.json")
-  }
+
+  def definition(): Action[AnyContent] =  Action.async {
+    Future.successful(Ok(
+      """
+        |{
+        |  "scopes": [],
+        |  "api": {
+        |    "name": "identity-verification-questions",
+        |    "description": "Backend service to provide question data and answer processing by unique user identifier for all verification services in MDTP",
+        |    "context": "identity-verification-questions",
+        |    "versions": [
+        |      {
+        |        "version": "1.0",
+        |        "status": "STABLE",
+        |        "endpointsEnabled": true
+        |      },
+        |      {
+        |        "version": "2.0",
+        |        "status": "STABLE",
+        |        "endpointsEnabled": true
+        |      }
+        |    ]
+        |  }
+        |}
+        |""".stripMargin)//.as(ContentTypes.withCharset(MimeTypes.JSON)(Codec.utf_8)))
+    )
+}
+
 
   def specification(version: String, file: String): Action[AnyContent] = {
-    assets.at(s"/public/api/conf/$version", file)
+    println(s"\n\nspecification Method")
+
+    CORSActionBuilder(configuration).async { implicit request =>
+      println(s"accessing file ${file}")
+
+      assets.at(s"/public/api/conf/$version", file)(request)
+    }
   }
 }
 
