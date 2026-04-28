300a51c2e96a18ad1fbe7c653d13c595f81d160a
Gaurav Sood <88654936+daglisood@users.noreply.github.com>
2026-04-28 14:23:00 +0100

    VER-4638: PR Bot Comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapPlayVersion = "10.5.0"
-  private val mongoVersion = "2.11.0"
+  private val bootstrapPlayVersion = "10.7.0"
+  private val mongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapPlayVersion,
